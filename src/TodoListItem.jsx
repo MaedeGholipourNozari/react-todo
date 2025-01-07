76e23816bfca5e74a3ac7b823c0b13b7564035a8
Maede Gholipour <maedegholipour@gmail.com>
2025-01-07 11:46:48 -0500

    Fix Remove button

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -6,7 +6,7 @@ const TodoListItem = ({ todo,onRemoveTodo }) => {
     return (
       <li>
         <h3>{todo.title}</h3>
-        <button type="button" onClick={() => onRemoveTodo(item.id)}>
+        <button type="button" onClick={() => onRemoveTodo(todo.id)}>
             Remove
           </button>
       </li>
